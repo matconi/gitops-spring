19659c3ae37f5eb75ddfa16b2cbf7d5ef3a09755
matconi <silvadaxeira@gmail.com>
2024-02-26 17:06:27 -0300

    iniciando com keycloak

--- module/src/main/java/domain/company/project/module/controllers/LessonController.java
+++ module/src/main/java/domain/company/project/module/controllers/LessonController.java
@@ -6,6 +6,7 @@ import domain.company.project.module.services.LessonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,6 +24,7 @@ public class LessonController {
     }
 
     @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<List<LessonResponse>> listLessons(){
         List<LessonResponse> lessones = lessonService.listLessons();
         return ResponseEntity.ok(lessones);
