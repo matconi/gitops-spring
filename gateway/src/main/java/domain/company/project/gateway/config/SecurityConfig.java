7c5a0eb8453f5d16d22098bdd49c7bee68a166ae
matconi <silvadaxeira@gmail.com>
2024-03-08 16:22:28 -0300

    adicionando cors nos servicos

--- gateway/src/main/java/domain/company/project/gateway/config/SecurityConfig.java
+++ gateway/src/main/java/domain/company/project/gateway/config/SecurityConfig.java
@@ -6,6 +6,10 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -13,13 +17,22 @@ public class SecurityConfig {
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
-        return serverHttpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable)
+        return serverHttpSecurity.cors(
+                    cors -> cors.configurationSource(request -> {
+                        CorsConfiguration corsConfiguration = new CorsConfiguration();
+                        corsConfiguration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+                        corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
+                        return corsConfiguration;
+                    })
+                )
                 .authorizeExchange(exchange -> exchange.pathMatchers(
-                        "/module/**", "/eureka/**", "/auth/**"
+                        "/module/**", "/eureka/**",
+                        "/auth/realms/realm/**"
                         ).permitAll()
                         .anyExchange().authenticated()
-                ).oauth2Client()
-                .and()
+                )
+                .oauth2Client(Customizer.withDefaults())
                 .oauth2Login(Customizer.withDefaults())
                 .build();
     }
--- module/src/main/java/domain/company/project/module/config/SecurityConfig.java
+++ module/src/main/java/domain/company/project/module/config/SecurityConfig.java
@@ -4,8 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableMethodSecurity
@@ -15,11 +19,21 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
         http
-        .csrf(AbstractHttpConfigurer::disable)
-        .oauth2ResourceServer(oauth2 -> oauth2
-                .jwt(jwt -> jwt.jwtAuthenticationConverter(new JWTConverter())));
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .cors(
+                cors -> cors.configurationSource(request -> {
+                    CorsConfiguration corsConfiguration = new CorsConfiguration();
+                    corsConfiguration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                    corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+                    corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
+                    return corsConfiguration;
+                })
+            )
+            .oauth2ResourceServer(oauth2 -> oauth2
+                    .jwt(jwt -> jwt.jwtAuthenticationConverter(new JWTConverter())));
 
         return http.build();
     }
